0052f7cff4f75bfc5f1197f635b3eb20d1e52d52
Pavel Klimov <pavelklimovmail@yandex.ru>
2019-05-13 02:34:59 +0300

    Исправление и дополнение верстки формы

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,8 @@ gulp.task("html", function () {
   .pipe(posthtml([
     include()
   ]))
-  .pipe(gulp.dest("build"));
+  .pipe(gulp.dest("build"))
+  .pipe(server.stream());
 });
 
 gulp.task("js", function () {
